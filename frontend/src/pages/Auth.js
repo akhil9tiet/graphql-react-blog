182fe756a5678a9e0aaa6c54c802079cf5309a8a
Akhil <akhil9tiet@gmail.com>
2020-01-05 17:31:16 -0800

    feat: successfull send backend signup data

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -2,16 +2,63 @@ import React, { Component } from 'react';
 import './Auth.css';
 
 class AuthPage extends Component {
+	constructor(props) {
+		super(props);
+		this.emailEl = React.createRef();
+		this.passwordEl = React.createRef();
+	}
+
+	submitHandler = (event) => {
+		event.preventDefault(); //to make sure no default gets sent
+		const email = this.emailEl.current.value;
+		const password = this.passwordEl.current.value;
+		//validation
+		if (email.trim().length === 0 || password.trim().length === 0) {
+			return;
+		}
+		// console.log(email, ":", password);
+
+		const requestBody = {
+			query: `
+      mutation{
+        createUser(userInput: {email: "${email}", password: "${password}"}) {
+          _id
+          email
+        }
+      }`
+		};
+
+		fetch('http://localhost:8000/graphql', {
+			method: 'POST',
+			body: JSON.stringify(requestBody),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+			.then((res) => {
+				if (res.status !== 200 && res.status !== 201) {
+					throw new Error('Failed!');
+				}
+				return res.json();
+			})
+			.then((resData) => {
+				console.log(resData);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
 	render() {
 		return (
-			<form className='auth-form'>
+			<form className='auth-form' onSubmit={this.submitHandler}>
 				<div className='form-control'>
 					<label htmlFor='email'>Email</label>
-					<input type='email' id='email' />
+					<input type='email' id='email' ref={this.emailEl} />
 				</div>
 				<div className='form-control'>
 					<label htmlFor='password'>Password</label>
-					<input type='password' id='password' />
+					<input type='password' id='password' ref={this.passwordEl} />
 				</div>
 				<div className='form-action'>
 					<button type='submit'>Submit</button>
